663a606542d96d9a37355481e4b61b09ca67afcf
bubbleboy14 <mario.balibrera@gmail.com>
2016-06-17 20:41:57 -0700

    enabled murder scraper

--- aemp/scrape.py
+++ aemp/scrape.py
@@ -2,7 +2,7 @@ from importlib import import_module
 from cantools.web import respond, succeed, fail, cgi_get
 
 def response():
-	scrape_target = cgi_get("scraper", choices=["lords", "fires", "buildings", "rent_control", "evictions"])
+	scrape_target = cgi_get("scraper", choices=["lords", "fires", "buildings", "rent_control", "evictions", "murders"])
 	import_module("scrapers.%s"%(scrape_target,)).full_scan()
 
 respond(response, threaded=True)
